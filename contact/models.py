35164de8118be6229bbed525fb9ef4a2e1881c1c
Mayzon <mayzon.santos@gmail.com>
2025-03-28 12:00:06 -0300

    Configurando o campo para recebimento das imagens e ULRs para exibição em navegador

--- contact/models.py
+++ contact/models.py
@@ -14,8 +14,9 @@ class Contact(models.Model):
     phone = models.CharField(max_length=50)
     email = models.EmailField(max_length=254, blank=True)
     created_date = models.DateTimeField(default=timezone.now())
-    description = models.TextField()
+    description = models.TextField(blank=True)
+    show = models.BooleanField(default=True)
+    picture = models.ImageField(blank=True, upload_to='pictures/%Y/%m/')
 
     def __str__(self) -> str:
         return f'{self.first_name} {self.last_name}'
-
