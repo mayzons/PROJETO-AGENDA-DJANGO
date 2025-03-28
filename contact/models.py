4550f1599265c6e1eda1bc332ae39627a435374b
Mayzon <mayzon.santos@gmail.com>
2025-03-28 13:00:12 -0300

    Criado a referencia de dono de contato

--- contact/models.py
+++ contact/models.py
@@ -1,8 +1,12 @@
 from django.db import models
 from django.utils import timezone
-
+from django.contrib.auth.models import User
 
 class Category(models.Model):
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+
     name = models.CharField(max_length=50)
 
     def __str__(self) -> str:
@@ -19,7 +23,12 @@ class Contact(models.Model):
     show = models.BooleanField(default=True)
     picture = models.ImageField(blank=True, upload_to='pictures/%Y/%m/')
     category = models.ForeignKey(
-        Category, 
+        Category,
+        on_delete=models.SET_NULL,
+        blank=True,
+        null=True)
+    owner = models.ForeignKey(
+        User,
         on_delete=models.SET_NULL,
         blank=True,
         null=True)
