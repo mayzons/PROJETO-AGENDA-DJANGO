eea4b1f426e80ea57dc6d54f3dd0fc39a2ea907e
Mayzon <mayzon.santos@gmail.com>
2025-03-29 10:39:52 -0300

    Add paginas na peqsquisa e contatos

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.db.models import Q
+from django.core.paginator import Paginator
 
 from contact.models import Contact
 
@@ -7,10 +8,13 @@ from contact.models import Contact
 def index(request):
     contacts = Contact.objects \
         .filter(show=True)\
-        .order_by('-id')[10:20]
+        .order_by('-id')
+    paginator = Paginator(contacts, 10)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
 
     context = {
-        'contacts': contacts,
+        'page_obj': page_obj,
         'site_title': 'Contatos - '
     }
 
@@ -37,8 +41,12 @@ def search(request):
             )\
         .order_by('-id')
 
+    paginator = Paginator(contacts, 10)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+
     context = {
-        'contacts': contacts,
+        'page_obj': page_obj,
         'site_title': 'Search - ',
         'search_value': search_value,
     }
