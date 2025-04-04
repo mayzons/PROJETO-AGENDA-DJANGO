216d89f688fbcf264e441f8f74137cdbfac5505a
Mayzon <mayzon.santos@gmail.com>
2025-04-04 14:03:34 -0300

    Versão final pré produção

--- contact/views/contact_forms.py
+++ contact/views/contact_forms.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.urls import reverse
+from django.contrib.auth.decorators import login_required
 
 from contact.forms import ContactForm
 from contact.models import Contact
 
 
+@login_required(login_url='contact:login')
 def create(request):
     form_action = reverse('contact:create')
 
@@ -16,7 +18,9 @@ def create(request):
         }
 
         if form.is_valid():
-            contact = form.save()
+            contact = form.save(commit=False)
+            contact.owner = request.user
+            contact.save()
             return redirect('contact:update', contact_id=contact.pk)
 
         return render(
@@ -37,8 +41,11 @@ def create(request):
     )
 
 
+@login_required(login_url='contact:login')
 def update(request, contact_id):
-    contact = get_object_or_404(Contact, pk=contact_id, show=True)
+    contact = get_object_or_404(
+        Contact, pk=contact_id, show=True, owner=request.user
+    )
     form_action = reverse('contact:update', args=(contact_id,))
 
     if request.method == 'POST':
@@ -67,4 +74,27 @@ def update(request, contact_id):
         request,
         'contact/create.html',
         context
+    )
+
+
+@login_required(login_url='contact:login')
+def delete(request, contact_id):
+
+    contact = get_object_or_404(
+        Contact, pk=contact_id, show=True, owner=request.user
+    )
+
+    confirmation = request.POST.get('confirmation', 'no')
+
+    if confirmation == 'yes':
+        contact.delete()
+        return redirect('contact:index')
+
+    return render(
+        request,
+        'contact/contact.html',
+        {
+            'contact': contact,
+            'confirmation': confirmation,
+        }
     )
--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -74,25 +74,3 @@ def contact(request, contact_id):
         'contact/contact.html',
         context
     )
-
-
-def delete(request, contact_id):
-
-    contact = get_object_or_404(
-        Contact, pk=contact_id, show=True
-    )
-
-    confirmation = request.POST.get('confirmation', 'no')
-
-    if confirmation == 'yes':
-        contact.delete()
-        return redirect('contact:index')
-
-    return render(
-        request,
-        'contact/contact.html',
-        {
-            'contact': contact,
-            'confirmation': confirmation,
-        }
-    )
